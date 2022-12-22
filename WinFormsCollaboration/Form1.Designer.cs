2a7571c69e5bd6088a6aafe66c20ac0917305076
Svebding <Sven.Hebding@it-bfw.de>
2022-12-22 08:53:42 +0100

    Update Form1.Designer.cs

--- WinFormsCollaboration/Form1.Designer.cs
+++ WinFormsCollaboration/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnLos = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnLos
+            // 
+            this.btnLos.Location = new System.Drawing.Point(249, 133);
+            this.btnLos.Name = "btnLos";
+            this.btnLos.Size = new System.Drawing.Size(73, 61);
+            this.btnLos.TabIndex = 0;
+            this.btnLos.Text = "Los!";
+            this.btnLos.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 361);
+            this.Controls.Add(this.btnLos);
             this.Name = "Form1";
             this.Text = "WinFormsCollaboration";
             this.ResumeLayout(false);
@@ -42,5 +53,7 @@
         }
 
         #endregion
+
+        private Button btnLos;
     }
 }
